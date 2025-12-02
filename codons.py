925f02a80052e0b2663268bcbad7d41664034c03
FixRay001 <bogdanzakharov2@gmail.com>
2025-12-02 14:08:42 +0200

    codon_dict

--- codons.py
+++ codons.py
@@ -1,4 +1,14 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    codon_dict = {}
 
+    with open(file_path, 'r') as f:
+        for line in f:
+            line = line.strip()
 
+            if not line:
+                continue
+
+            codon, amino = line.split()
+            codon_dict[codon] = amino
+
+    return codon_dict
